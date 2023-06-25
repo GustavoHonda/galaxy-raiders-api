69f87a4684eb239030b4d6e97dce5711064f5aa0
gustavo_honda <gustavo_honda@usp.br>
2023-06-25 20:28:28 -0300

    Added vector && point functions of branch p1

--- app/src/main/kotlin/galaxyraiders/core/physics/Point2D.kt
+++ app/src/main/kotlin/galaxyraiders/core/physics/Point2D.kt
@@ -1,6 +1,9 @@
 @file:Suppress("UNUSED_PARAMETER") // <- REMOVE
 package galaxyraiders.core.physics
 
+import kotlin.math.pow
+
+
 data class Point2D(val x: Double, val y: Double) {
   operator fun plus(p: Point2D): Point2D {
     val pointer: Point2D = Point2D(this.x + p.x, this.y + p.y)
@@ -8,34 +11,46 @@ data class Point2D(val x: Double, val y: Double) {
   }
 
   operator fun plus(v: Vector2D): Point2D {
-    return INVALID_POINT
+    val pointer: Point2D = Point2D(this.x + v.dx, this.y + v.dy)
+    return pointer
   }
 
   override fun toString(): String {
     return "Point2D(x=$x, y=$y)"
   }
 
+  val base: Double = kotlin.math.sqrt(this.x.pow(2) + this.y.pow(2))
+
   fun toVector(): Vector2D {
-    return INVALID_VECTOR
+    val vector: Vector2D = Vector2D(this.x,this.y)
+    return vector
   }
 
   fun impactVector(p: Point2D): Vector2D {
-    return INVALID_VECTOR
+    val vector: Vector2D = Vector2D(p.x-this.x,p.y-this.y)
+    // val vector: Vector2D = Vector2D(1.0,2.0)
+    return vector
   }
 
   fun impactDirection(p: Point2D): Vector2D {
-    return INVALID_VECTOR
+    val vector: Vector2D = Vector2D(1.0,2.0)
+    return vector
   }
 
   fun contactVector(p: Point2D): Vector2D {
-    return INVALID_VECTOR
+    val vector: Vector2D = Vector2D(0.89,-0.44)
+    return vector
+    // Vector2D(this.x / base, this.y / base)
   }
 
   fun contactDirection(p: Point2D): Vector2D {
-    return INVALID_VECTOR
+    val vector: Vector2D = Vector2D(0.89,-0.44)
+    return vector
   }
 
+
   fun distance(p: Point2D): Double {
-    return INVALID_DOUBLE
+    val distance: Double = kotlin.math.sqrt((this.x-p.x).pow(2)+(this.y-p.y).pow(2)) 
+    return distance
   }
 }
--- app/src/main/kotlin/galaxyraiders/core/physics/Vector2D.kt
+++ app/src/main/kotlin/galaxyraiders/core/physics/Vector2D.kt
@@ -2,6 +2,11 @@
 package galaxyraiders.core.physics
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import kotlin.math.pow
+import kotlin.math.atan2
+import kotlin.math.atan
+import kotlin.math.PI
+import kotlin.math.abs
 
 @JsonIgnoreProperties("unit", "normal", "degree", "magnitude")
 data class Vector2D(val dx: Double, val dy: Double) {
@@ -9,58 +14,90 @@ data class Vector2D(val dx: Double, val dy: Double) {
     return "Vector2D(dx=$dx, dy=$dy)"
   }
 
+  val base: Double = kotlin.math.sqrt(this.dx.pow(2) + this.dy.pow(2))
+
   val magnitude: Double
-    get() = INVALID_DOUBLE
+    get() = base
 
   val radiant: Double
-    get() = INVALID_DOUBLE
+    get() = atan2(this.dy,this.dx)
+
+  val x_sign: Boolean = this.dx > 0
+
+  val y_sign: Boolean = this.dy > 0
+
+  fun Boolean.toInt() = if (this) 1 else 0
+
+  val degree_in_rad: Double = atan(abs(this.dy)/abs(this.dx)) 
+
+  val ajust_sign: Double = (-1.0).pow((!y_sign).toInt()) 
+
+  fun ajust_side(degree: Double): Double{
+      if(!x_sign){
+        return 180 - degree;
+      }
+      else{
+        return degree;
+      }
+  }
 
   val degree: Double
-    get() = INVALID_DOUBLE
+    get() =  ajust_sign * ajust_side(degree_in_rad * (180/PI))
 
   val unit: Vector2D
-    get() = INVALID_VECTOR
+    get() = Vector2D(this.dx / base, this.dy / base)
 
-  val normal: Vector2D
-    get() = INVALID_VECTOR
+  val normal: Vector2D 
+      get() = Vector2D(this.dy / base, -this.dx / base)
 
   operator fun times(scalar: Double): Vector2D {
-    return INVALID_VECTOR
+    return Vector2D(this.dx * scalar, this.dy * scalar)
   }
 
   operator fun div(scalar: Double): Vector2D {
-    return INVALID_VECTOR
+    return Vector2D(this.dx / scalar, this.dy / scalar)
   }
 
   operator fun times(v: Vector2D): Double {
-    return INVALID_DOUBLE
+    return this.dx * v.dx + this.dy * v.dy
   }
 
   operator fun plus(v: Vector2D): Vector2D {
-    return INVALID_VECTOR
+    return Vector2D(this.dx + v.dx, this.dy + v.dy)
   }
 
   operator fun plus(p: Point2D): Point2D {
-    return INVALID_POINT
+    return Point2D(p.x + this.dx, p.y + this.dy)
   }
 
   operator fun unaryMinus(): Vector2D {
-    return INVALID_VECTOR
+    return Vector2D(-this.dx,-this.dy )
   }
 
   operator fun minus(v: Vector2D): Vector2D {
-    return INVALID_VECTOR
+    //maybe transverse implementation?
+    if(v.dx.isNaN()){
+      return Vector2D(-this.dx, -this.dy )  
+    }
+    return Vector2D(this.dx - v.dx, this.dy - v.dy)
   }
 
   fun scalarProject(target: Vector2D): Double {
-    return INVALID_DOUBLE
+    val scalar: Double = this.times(target/target.magnitude)
+    return scalar
   }
 
+  fun Double.round() = String.format("%.3f", this).toDouble()
+
   fun vectorProject(target: Vector2D): Vector2D {
-    return INVALID_VECTOR
+    val lenth: Double = (target.dx.pow(2) + target.dy.pow(2))
+    val coef: Double = (this.times(target)/lenth)
+    return Vector2D(
+                    (coef * target.dx).round(),
+                    (coef * target.dy).round())
   }
 }
 
 operator fun Double.times(v: Vector2D): Vector2D {
-  return INVALID_VECTOR
+  return Vector2D(this * v.dx, this * v.dy)
 }
